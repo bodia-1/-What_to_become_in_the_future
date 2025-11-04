9cdeecc3618ecd5d5c87f0385ae273f021d1904a
Bogdan <lozhnikovbogdan2011@gmail.com>
2025-11-04 22:09:05 +0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+// vatna
+
+// VANTA.GLOBE({
+//   el: "#vanta",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: true,
+//   minHeight: 1200.00,
+//   minWidth: 1200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00,
+//   color: 0x0,
+//   color2: 0x0,
+//   backgroundColor: 0xffffff
+// })
+
+VANTA.CLOUDS({
+  el: "#vanta",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  sunColor: 0xfafafa,
+  sunGlareColor: 0x55403b,
+  sunlightColor: 0x3c210a
+})
+
+//aos
+
+AOS.init();
